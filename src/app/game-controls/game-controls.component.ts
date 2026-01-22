7e3f67c0be5e1765823ddc2c154e879c007fe88b
Daniel Filho <danielfilhoce@gmail.com>
2026-01-22 00:06:48 -0500

    adding more visual queues

--- src/app/game-controls/game-controls.component.ts
+++ src/app/game-controls/game-controls.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, linkedSignal, effect } from '@angular/core';
 import { GameStore } from '../stores';
 import { IconComponent } from '../icon/icon.component';
 import { Language } from '../models';
@@ -12,5 +12,48 @@ import { Language } from '../models';
 export class GameControlsComponent {
   protected readonly Language? = Language;
 
-  constructor(protected readonly gameStore: GameStore) {}
+  protected isGameMenuOpen = signal(true);
+  protected isLanguageSelectorOpen = signal(true);
+
+  languages = signal(
+    [
+      { code: Language.PT_BR, label: 'PT' },
+      { code: Language.EN_US, label: 'EN' },
+      // { code: Language.FR, label: 'FR' },
+    ].sort((a, b) => a.label.localeCompare(b.label)),
+  );
+
+  constructor(protected readonly gameStore: GameStore) {
+    effect(() => {
+      const currentLanguage = this.gameStore.language();
+      this.setLanguages(currentLanguage);
+    });
+
+    setTimeout(() => {
+      this.isGameMenuOpen.set(false);
+      this.isLanguageSelectorOpen.set(false);
+    }, 1500);
+  }
+
+  selectLanguage(language: Language, event?: Event) {
+    if (this.isLanguageSelectorOpen()) {
+      event?.stopPropagation();
+
+      this.gameStore.setLanguage(language);
+
+      this.isLanguageSelectorOpen.set(false);
+    }
+  }
+  private setLanguages(language: Language) {
+    this.languages.update((langs) => {
+      return [
+        langs.find((lang) => lang.code === language)!,
+        ...langs.filter((lang) => lang.code !== language),
+      ];
+    });
+  }
+
+  protected toggleLanguageSelector() {
+    this.isLanguageSelectorOpen.update((isOpen) => !isOpen);
+  }
 }
--- src/app/models/language.ts
+++ src/app/models/language.ts
@@ -1,4 +1,5 @@
 export enum Language {
   EN_US = 'en-us',
   PT_BR = 'pt-br',
+  FR = 'fr',
 }
